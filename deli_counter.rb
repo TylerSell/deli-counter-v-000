0a775392ebd1443490528c45500bff4ecb74e17b
tylerselldesign@gmail.com <tylerselldesign@gmail.com>
2018-08-18 15:29:23 +0000

    automatically backed up by learn

--- deli_counter.rb
+++ deli_counter.rb
@@ -24,8 +24,6 @@ def now_serving(katz_deli)
   if katz_deli.empty?
     puts "There is nobody waiting to be served!"
   else 
-    katz_deli.each do |name|
-    puts "Currently serving #{name}."
-    end
+    puts "Currently serving #{katz_deli.shift}."
   end
 end

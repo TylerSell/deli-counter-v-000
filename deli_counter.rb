70b72811d504b84f7fc32f4e2928025fd5d196e8
tylerselldesign@gmail.com <tylerselldesign@gmail.com>
2018-08-18 14:50:46 +0000

    automatically backed up by learn

--- deli_counter.rb
+++ deli_counter.rb
@@ -1,4 +1,23 @@
+katz_deli = []
+
 def line 
   
 end
 
+def take_a_number(katz_deli)
+  counter = 1 
+  katz_deli.each do |name|
+    puts "Welcome #{name}. You are number #{counter} in line."
+    counter += 1 
+  end
+end
+
+def now_serving(katz_deli)
+  katz_deli.each do |name|
+  if katz_deli == nil
+    puts "There is nobody waiting to be served!"
+  else 
+    puts "Currently serving #{name}"
+    end
+  end
+end

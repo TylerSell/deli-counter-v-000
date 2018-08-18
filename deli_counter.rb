d8a67b818ff452510816b1dd54a007b4e147fd9a
tylerselldesign@gmail.com <tylerselldesign@gmail.com>
2018-08-18 15:18:08 +0000

    automatically backed up by learn

--- deli_counter.rb
+++ deli_counter.rb
@@ -2,7 +2,6 @@ katz_deli = []
 
 def line(katz_deli)
   message = "The line is currently: "
-  katz_deli.each_with_index do |name|
     if katz_deli.empty?
       puts "The line is currently empty."
     else 
